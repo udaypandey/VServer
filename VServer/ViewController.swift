d033387f95e80dfde247dfe25aa774b347ab70d7
Uday Pandey <uday.pandey@tsb.co.uk>
2020-03-03 20:55:56 +0000

    Added initial project files in

---
+++ VServer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VServer
+//
+//  Created by Uday Pandey on 03/03/2020.
+//  Copyright © 2020 Thirstysea Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
